44bda31c218456e31511775c8744e8237ff4c281
Vincent Klimjack <vklimjack@gmail.com>
2020-11-20 13:28:06 +0000

    added delete method and helper method

--- tree.rb
+++ tree.rb
@@ -23,12 +23,38 @@ class Tree
     return Node.new(value) if node.nil?
     return node if node.data == value
 
-    if node.data < value
-      node.right = insert(value, node.right)
+    node.data < value ? node.right = insert(value, node.right) : node.left = insert(value, node.left)
+    node
+  end
+
+  def delete(value, node = @root)
+    return node if node.nil?
+
+    if value < node.data
+      node.left = delete(value, node.left)
+    elsif value > node.data
+      node.right = delete(value, node.right)
     else
-      node.left = insert(value, node.left)
+      if node.left.nil?
+        temp = node.right
+        node = nil
+        return temp
+      elsif node.right.nil?
+        temp = node.left
+        node = nil
+        return temp
+      end
+      temp = min_value_node(node.right)
+      node.data = temp.data
+      node.right = delete(temp.data, node.right)
     end
     node
+  end
+
+  def min_value_node(node)
+    current = node
+    current = current.left while current.left
+    current
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -45,4 +71,8 @@ bst.pretty_print
 bst.insert(0)
 bst.pretty_print
 bst.insert(100)
+bst.pretty_print
+bst.delete(0)
+bst.pretty_print
+bst.delete(4)
 bst.pretty_print
